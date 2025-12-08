57955c075a4e4a3c2670479814e2ede62b9b21ce
Yash840 <yashjawle440@gmail.com>
2025-12-08 19:41:24 +0530

    feat(security): add static factory methods in Permission entity
    Factory Methods Added:
    - of(PermissionName)
    - of(PermissionName, List<Role>)

--- src/main/java/org/cross/medicore/security/internals/entities/Permission.java
+++ src/main/java/org/cross/medicore/security/internals/entities/Permission.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.cross.medicore.security.internals.constants.PermissionName;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -28,6 +29,17 @@ public class Permission {
     public void addRole(Role role){
         if(!Objects.isNull(role))
             roles.add(role);
+    }
+
+    public static Permission of(PermissionName name){
+        return new Permission(name);
+    }
+
+    public static Permission of(PermissionName name, List<Role> roles){
+        Permission permission =  Permission.of(name);
+        roles.forEach(permission::addRole);
+
+        return permission;
     }
 
     // TODO : Add toGrantedAuthority Method.
